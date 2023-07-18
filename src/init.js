06245c16daf26a41a180bd628196f020d6eb3724
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-07-18 21:09:09 +0200

    add init

---
+++ src/init.js
@@ -0,0 +1,44 @@
+const watch = document.querySelector('#watch');
+
+document.querySelector('#buttons').addEventListener('click', (element) => {
+    if(element.target.id === 'start'){
+        start();
+    } else if (element.target.id === 'pause') {
+        pause();
+    } else if (element.target.id === 'reset') {
+        reset();
+    }
+});
+
+const date = new Date();
+let timerInterval;
+
+const showTime = () => {
+    const minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    const milliseconds = date.getMilliseconds() < 10 ? `00${date.getMilliseconds()}` : date.getMilliseconds();
+    watch.innerHTML = `${minutes}:${seconds}:${milliseconds}`;
+
+}
+
+const start = () => {
+    timerInterval = setInterval(() => {
+        date.setMilliseconds(date.getMilliseconds() + 1);
+        showTime();
+    }, 1);
+}
+
+
+const pause = () => {
+    clearInterval(timerInterval);
+}
+
+const reset = () => {
+    date.setMilliseconds(0);
+    date.setSeconds(0);
+    date.setMinutes(0);
+    showTime();
+}
+
+showTime();
+reset();
