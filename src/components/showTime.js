b02fae05b7e6ae577477fe9dff45e5ab1bd865b0
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-07-19 15:24:08 +0200

    update showTime

--- src/components/showTime.js
+++ src/components/showTime.js
@@ -1,8 +1,30 @@
+// import { data, dom } from "../data.js";
+
+// const showTime = () => {
+//   dom.minutes.innerHTML = data.minutes;
+//   dom.seconds.innerHTML = data.seconds;
+//   dom.milliseconds.innerHTML = data.milliseconds;
+// };
+
+// export default showTime;
+
+
 import { data, dom } from "../data.js";
 
+// let lastMinute = null;
+// let lastSecond = null;
+
 const showTime = () => {
-  dom.minutes.innerHTML = data.minutes;
-  dom.seconds.innerHTML = data.seconds;
+  if (data.minutes !== data.lastMinute) {
+    dom.minutes.innerHTML = data.minutes;
+    data.lastMinute = data.minutes;
+  }
+
+  if (data.seconds !== data.lastSecond) {
+    dom.seconds.innerHTML = data.seconds;
+    data.lastSecond = data.seconds;
+  }
+
   dom.milliseconds.innerHTML = data.milliseconds;
 };
 
